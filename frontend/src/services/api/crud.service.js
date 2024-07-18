720230a5155db7771bfb4d95e365bd2a0f35007b
LazarevDS <lazarevds@uriit.ru>
2024-07-18 16:17:41 +0500

    Module 5 finished

---
+++ frontend/src/services/api/crud.service.js
@@ -0,0 +1,23 @@
+import { ApiService } from "@/services/api/api.service";
+
+export class CrudService extends ApiService {
+  constructor(resource) {
+    super();
+    this.reverse = resource;
+  }
+
+  get() {
+    return this.$get(this.resource);
+  }
+
+  post(entity) {
+    return this.$post(this.resource, entity);
+  }
+  put(entity) {
+    return this.$put(`${this.resource}/${entity.id}`, entity);
+  }
+
+  delete(id) {
+    return this.$delete(`${this.resource}/${id}`);
+  }
+}
